0a33bc5f185bbaf00830e55ca885e907532da6eb
Alex Odom <alexjodom@gmail.com>
2021-02-26 19:41:51 -0500

    Fixed checkstyle warnings

--- src/main/java/frc/robot/drivers/FXSwerveModule.java
+++ src/main/java/frc/robot/drivers/FXSwerveModule.java
@@ -1,56 +1,49 @@
 package frc.robot.drivers;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 
 /**
- * A hardware wrapper class for a swerve module
+ * A hardware wrapper class for a swerve module.
  */
-public class FXSwerveModule implements SwerveModule {   
+public class FxSwerveModule implements SwerveModule {
     
-    private final WPI_TalonFX wheelMotor_;
-    private final WPI_TalonFX steerMotor_;
-    
-    /**
-     * Constructs a swerve modules
-     * @param steerID 
-     *  The CAN ID of the Spark Max used to control the steering
-     * @param wheelID
-     *  The CAN Id of the Spark Max used to control the wheel
-     * @param encoderId
-     *  The CAN ID of the CANCoder used to determine the angle of the module
-     * @param steeringReversed
-     *  Wether or not the motor used for steering is reversed
-     * @param wheelReversed
-     *  Wether or not the motor used for the wheel is reversed
-     */
-    public FXSwerveModule(int steerID, int wheelID, int encoderId,
+  private final WPI_TalonFX wheelMotor;
+  private final WPI_TalonFX steerMotor;
+  
+  /**
+   * Constructs a swerve modules.
+   *
+   * @param steerId The CAN ID of the Spark Max used to control the steering
+   * @param wheelId The CAN Id of the Spark Max used to control the wheel
+   * @param encoderId The CAN ID of the CANCoder used to determine the angle of the module
+   * @param steeringReversed Wether or not the motor used for steering is reversed
+   * @param wheelReversed Wether or not the motor used for the wheel is reversed
+   */
+  public FxSwerveModule(int steerId, int wheelId, int encoderId,
         boolean steeringReversed, boolean wheelReversed) {
-        wheelMotor_ = new WPI_TalonFX(wheelID);
-        steerMotor_ = new WPI_TalonFX(steerID);
-    }
+    steerMotor = new WPI_TalonFX(steerId);
+    wheelMotor = new WPI_TalonFX(wheelId);
+  }
 
-    /**
-     * Constructs a swerve modules
-     * @param steeringID 
-     *  The CAN ID of the Spark Max used to control the steering
-     * @param wheelID
-     *  The CAN Id of the Spark Max used to control the wheel
-     * @param encoderId
-     *  The CAN ID of the CANCoder used to determine the angle of the module
-     */
-    public FXSwerveModule(int steeringID, int wheelID, int encoderID) {
-        this(steeringID, wheelID, encoderID, false, false);
-    }
+  /**
+   * Constructs a swerve modules.
+   *
+   * @param steerId The CAN ID of the Spark Max used to control the steering
+   * @param wheelId The CAN Id of the Spark Max used to control the wheel
+   * @param encoderId The CAN ID of the CANCoder used to determine the angle of the module
+   */
+  public FxSwerveModule(int steerId, int wheelId, int encoderId) {
+    this(steerId, wheelId, encoderId, false, false);
+  }
 
-    @Override
-    public SwerveModuleState getState() {
-        return new SwerveModuleState();
-    }
+  @Override
+  public SwerveModuleState getState() {
+    return new SwerveModuleState();
+  }
 
-    @Override
-    public void setState(SwerveModuleState state) {
-        
-    }
+  @Override
+  public void setState(SwerveModuleState state) {
+    
+  }
 }
--- src/main/java/frc/robot/drivers/SwerveModule.java
+++ src/main/java/frc/robot/drivers/SwerveModule.java
@@ -2,18 +2,23 @@ package frc.robot.drivers;
 
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 
+/**
+ * This interface defines the api needed to control a swerve module fully.
+ */
 public interface SwerveModule {
 
-    /**
-     * Get the current state of the module
-     * @return A SwerveModuleState representing the current speed
-     *  and rotation of the module
-     */
-    public SwerveModuleState getState();
+  /**
+   * Get the current state of the module.
+   *
+   * @return A SwerveModuleState representing the current speed
+   *     and rotation of the module
+   */
+  public SwerveModuleState getState();
 
-    /**
-     * Set the current state of the swerve module
-     * @param state The SwerveModuleState to set the module to
-     */
-    public void setState(SwerveModuleState state);
+  /**
+   * Set the current state of the swerve module.
+   *
+   * @param state The SwerveModuleState to set the module to
+   */
+  public void setState(SwerveModuleState state);
 }
