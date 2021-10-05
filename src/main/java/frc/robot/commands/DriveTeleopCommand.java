89f89309787f5f8bbf7c6d687a9fa6e7d6e06e20
Alex Odom <alexjodom@gmail.com>
2021-10-05 18:09:23 -0400

    Added Joystick curves.

--- src/main/java/frc/robot/commands/DriveTeleopCommand.java
+++ src/main/java/frc/robot/commands/DriveTeleopCommand.java
@@ -9,16 +9,15 @@ import java.util.function.Supplier;
 /** Command to drive the swerve in teleop. Supplied left joystick x and y, and right joystick x. */
 public class DriveTeleopCommand extends CommandBase {
 
-  private static final double maxTranslationalVelocityRate = 4; // m/s per second
-  private static final double maxRotationVelocityRate = 2 * Math.PI; // rads/s per second
-  private static final double maxTranslationalVelocity = Swerve.maxVelocity;
-  private static final double maxRotationalVelocity = Swerve.maxRotationalVelocity;
+  private static final double maxTranslationalVelocityRate = 10; // m/s per second
+  private static final double maxRotationVelocityRate = 4 * Math.PI; // rads/s per second
 
-  private static final double dxDeadband = 0.075; // Joystick percentage
-  private static final double dyDeadband = 0.075; // Joystick percentage
-  private static final double drDeadband = 0.075; // Joystick percentage
+  private static final double DEADBAND = 0.2;
 
   private final Swerve swerve;
+
+  private final double translationCurveStrength = 0.4;
+  private final double rotationCurveStrength = 10.0; // 10.0 makes it effectively linear.
 
   private final Supplier<Double> vxSupplier;
   private final Supplier<Double> vySupplier;
@@ -76,16 +75,16 @@ public class DriveTeleopCommand extends CommandBase {
   @Override
   public void execute() {
     // Get driver input
-    double dx = vxRateLimiter.calculate(vxSupplier.get() * maxTranslationalVelocity);
-    double dy = vyRateLimiter.calculate(vySupplier.get() * maxTranslationalVelocity);
-    double dr = vrRateLimiter.calculate(vrSupplier.get() * maxRotationalVelocity);
+    double dx = vxRateLimiter.calculate(vxSupplier.get() * Swerve.maxVelocity);
+    double dy = vyRateLimiter.calculate(vySupplier.get() * Swerve.maxVelocity);
+    double dr = vrRateLimiter.calculate(vrSupplier.get() * Swerve.maxRotationalVelocity);
     SmartDashboard.putNumber("Swerve vX", dx);
     SmartDashboard.putNumber("Swerve vY", dy);
     SmartDashboard.putNumber("Swerve vR", dr);
 
-    dx = (Math.abs(dx) > dxDeadband) ? dx : 0;
-    dy = (Math.abs(dy) > dyDeadband) ? dy : 0;
-    dr = (Math.abs(dr) > drDeadband) ? dr : 0;
+    dx = applyControlCurve(Math.abs(dx) > DEADBAND ? dx : 0, translationCurveStrength);
+    dy = applyControlCurve(Math.abs(dy) > DEADBAND ? dy : 0, translationCurveStrength);
+    dr = applyControlCurve(Math.abs(dr) > DEADBAND ? dr : 0, rotationCurveStrength);
 
     swerve.drive(dx, dy, dr, fieldOriented);
   }
@@ -98,5 +97,28 @@ public class DriveTeleopCommand extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     swerve.stop();
+  }
+
+  /**
+   * Applys a controll curve to the given input. This function does two things:
+   *
+   * <p>1. Remove the effect of the deadband.
+   *
+   * <p>2. Map the control signal onto a normalized signum function.
+   *
+   * @param controlValue The control signal to map.
+   * @param curveStrength The strength of the signum function. Note that values between -1.0 and 0.0
+   *     will lead to bad results. Go to See <a
+   *     href="https://www.desmos.com/calculator/zzpkh9cujz">Desmos</a> to see how curveStrength (k)
+   *     affects the output.
+   * @return The mapped control value.
+   */
+  private double applyControlCurve(double controlValue, double curveStrength) {
+    // Get rid of the discontinuity that is caused by setting any value between -DEADBAND and
+    // DEADBAND to 0.
+    final double absControl = Math.abs(controlValue);
+    final double continuousControl = (absControl - DEADBAND) * (1.0 / (1.0 - DEADBAND));
+    return Math.signum(continuousControl)
+        * ((curveStrength * continuousControl) / (curveStrength - continuousControl + 1));
   }
 }
