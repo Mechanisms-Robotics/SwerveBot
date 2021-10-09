0dd6bf9b8db712d940a55dfa932f5a7ac4cad1dc
napro1299 <nick.b.amato@gmail.com>
2021-10-09 18:56:43 -0400

    photonvision limelight subsystem

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -3,62 +3,36 @@ package frc.robot.subsystems;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import org.photonvision.common.hardware.VisionLEDMode;
+import org.photonvision.targeting.PhotonPipelineResult;
 
 /**
  * This acts as a wrapper class for our Limelight. It handles targeting so our shooter can aim at
  * the power port. *
  */
 public class Limelight extends SubsystemBase {
-  private final NetworkTable llNetworkTable =
-      NetworkTableInstance.getDefault().getTable("limelight");
 
-  private static class LLRawData {
-    /* Inputs */
-    public int currentLEDMode;
-    public int currentPipeline;
-    public double xOffset;
-    public double yOffset;
-    public boolean hasTarget;
-
-    /* Outputs */
-    public int wantedLEDMode = 1;
-    public int wantedPipeline = 0;
-  }
-
-  public enum LedMode {
-    PIPELINE, OFF, BLINK, ON
-  }
-
-  private final LLRawData rawData = new LLRawData();
+  private PhotonCamera camera = new PhotonCamera("limelight");
+  private PhotonPipelineResult result = new PhotonPipelineResult();
 
   public Limelight() {}
 
   @Override
   public void periodic() {
     /* Read periodic inputs */
-    rawData.currentLEDMode = (int) llNetworkTable.getEntry("ledMode").getDouble(1.0);
-    rawData.currentPipeline = (int) llNetworkTable.getEntry("pipeline").getDouble(0.0);
-    rawData.xOffset = llNetworkTable.getEntry("tx").getDouble(0.0);
-    rawData.yOffset = llNetworkTable.getEntry("ty").getDouble(0.0);
-    rawData.hasTarget = (llNetworkTable.getEntry("tv").getDouble(0.0) == 1.0);
-
-    /* Write periodic outputs */
-    if (rawData.currentLEDMode != rawData.wantedLEDMode
-        || rawData.currentPipeline != rawData.wantedPipeline) {
-      llNetworkTable.getEntry("ledMode").setNumber(rawData.wantedLEDMode);
-      llNetworkTable.getEntry("pipeline").setNumber(rawData.wantedPipeline);
-    }
+    result = camera.getLatestResult();
   }
 
-  public void setLEDMode(LedMode ledMode) {
-    rawData.wantedLEDMode = ledMode.ordinal();
+  public PhotonPipelineResult getResult() {
+    return result;
   }
 
-  public void setLEDMode(int ledMode) {
-    rawData.wantedLEDMode = ledMode;
+  public void setLEDMode(VisionLEDMode mode) {
+    camera.setLED(mode);
   }
 
   public void setPipeline(int pipeline) {
-    rawData.wantedPipeline = pipeline;
+    camera.setPipelineIndex(pipeline);
   }
 }
